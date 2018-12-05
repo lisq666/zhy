36346de18f51d3b729a4a754165b7edf9b39b632
lisq666 <501309186@qq.com>
2018-12-05 15:52:38 +0800

    智慧云

--- spring-boot-dubbo/src/main/java/com/example/ServiceApplication.java
+++ spring-boot-dubbo/src/main/java/com/example/ServiceApplication.java
@@ -4,12 +4,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @MapperScan("com.example.mapper")
 @ImportResource({"classpath:dubbo/*.xml"})
 @EnableTransactionManagement
 @SpringBootApplication
+@EnableAsync
 public class ServiceApplication {
 
 	public static void main(String[] args) {
