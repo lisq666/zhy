6b2cd2c5ea5f4b6e5bbddafcf038e407017f44c3
lsq <501309186@qq.com>
2019-03-08 15:08:29 +0800

    智慧云

--- spring-boot-dubbo/src/main/java/com/example/service/AsyncServiceImpl.java
+++ spring-boot-dubbo/src/main/java/com/example/service/AsyncServiceImpl.java
@@ -5,24 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 @Slf4j
 @Service("asyncService")
 public class AsyncServiceImpl implements AsyncService{
 
-
     @Override
     @Async("asyncServiceExecutor")
     public void executeAsync() {
-        log.info("start executeAsync");
         try{
             Thread.sleep(5000);
         }catch(Exception e){
             e.printStackTrace();
         }
-        log.info("end executeAsync");
     }
 
     public static void main(String[] args) throws Exception{
